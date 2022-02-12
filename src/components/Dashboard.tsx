4072142f8137306974f6da68b98de5809065bbef
[aviv polak] <[avivp0437@gmail.com]>
2022-02-12 22:10:49 +0200

    basic without alerts and search

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,38 +1,15 @@
-import React from "react";
-// import ChartistGraph from "react-chartist";
-import { PieChart } from "react-minimal-pie-chart";
-
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import Header from "./Header";
 import Pie from "./Pie";
-const getRandomColor = () => {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-};
-
-const dataMock:any[] = [];
 
 export const Dashboard = () => {
     const [stats, setStats] = useState([]);
-    const options = {
-        width: "1000px",
-        height: "1000px",
-        donut: true,
-        donutWidth: 200,
-    };
 
-    const updateWord = async () => {
+    const updateStats = async () => {
         try {
             const response = await axios.get(`http://localhost:3000/stats`);
             if (response.data) {
-          
-                
                 setStats(response.data.data);
             }
         } catch (err) {
@@ -40,42 +17,16 @@ export const Dashboard = () => {
         }
     };
     useEffect(() => {
-        console.log("useEffect");
-        updateWord();
+        updateStats();
     }, []);
 
     return (
         <div>
             <Header />
+            <div className="pie">
             <Pie data={stats} />
-            {/* <PieChart
-                data={[
-                    {
-                        label: "JavaScript",
-                        value: 1,
-                        color: getRandomColor(),
-                    },
-                    {
-                        label: "HTML",
-                        value: 2,
-                        color: getRandomColor(),
-                    },
-                    // { title: "HTML", value: stats.html, color: "#00FF00" },
-                    // { title: "CSS", value: stats.css, color: "#0000FF" },
-                    // {
-                    //     title: "TypeScript",
-                    //     value: stats.typescript,
-                    //     color: "#FFFF00",
-                    // },
-                    // { title: "C#", value: stats.csharp, color: "#FF00FF" },
-                    // { title: "C++", value: stats.cpp, color: "#00FFFF" },
-                    // { title: "Python", value: stats.python, color: "#000000" },
-                    // { title: "Java", value: stats.java, color: "#FFFFFF" },
-                ]}
-                // data={stats}
-                // options={options}
-                // type="Pie"
-            /> */}
+            </div>
+           
         </div>
     );
 };
--- src/components/Pie.tsx
+++ src/components/Pie.tsx
@@ -1,57 +1,59 @@
-import React, { useState, ComponentProps } from 'react';
+import React, { useState, ComponentProps } from "react";
 import { PieChart } from "react-minimal-pie-chart";
 
 type Props = {
-  data: ComponentProps<typeof PieChart>['data'];
+    data: ComponentProps<typeof PieChart>["data"];
 };
 
 function Pie(props: Props) {
-  const [selected, setSelected] = useState<number | undefined>(0);
-  const [hovered, setHovered] = useState<number | undefined>(undefined);
+    const [selected, setSelected] = useState<number | undefined>(undefined);
+    const [hovered, setHovered] = useState<number | undefined>(undefined);
 
-  const data = props.data.map((entry, i) => {
-    if (hovered === i) {
-      return {
-        ...entry,
-        color: 'grey',
-      };
-    }
-    return entry;
-  });
+    const data = props.data.map((entry, i) => {
+        if (hovered === i) {
+            return {
+                ...entry,
+                color: "grey",
+            };
+        }
+        return entry;
+    });
 
-  const lineWidth = 60;
+    const lineWidth = 60;
 
-  return (
-    <PieChart
-      style={{
-        fontFamily:
-          '"Nunito Sans", -apple-system, Helvetica, Arial, sans-serif',
-        fontSize: '8px',
-      }}
-      data={data}
-      radius={PieChart.defaultProps.radius - 6}
-      lineWidth={60}
-      segmentsStyle={{ transition: 'stroke .3s', cursor: 'pointer' }}
-      segmentsShift={(index) => (index === selected ? 6 : 1)}
-      animate
-      label={({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
-      labelPosition={100 - lineWidth / 2}
-      labelStyle={{
-        fill: '#fff',
-        opacity: 0.75,
-        pointerEvents: 'none',
-      }}
-      onClick={(_, index) => {
-        setSelected(index === selected ? undefined : index);
-      }}
-      onMouseOver={(_, index) => {
-        setHovered(index);
-      }}
-      onMouseOut={() => {
-        setHovered(undefined);
-      }}
-    />
-  );
+    return (
+        <PieChart
+            style={{
+                fontFamily:
+                    '"Nunito Sans", -apple-system, Helvetica, Arial, sans-serif',
+                fontSize: "1.5px",
+            }}
+            data={data}
+            radius={PieChart.defaultProps.radius - 35}
+            lineWidth={100}
+            segmentsStyle={{ transition: "stroke .3s", cursor: "pointer" }}
+            segmentsShift={(index) => (index === selected ? 6 : 1)}
+            animate
+            label={({ dataEntry }) =>
+                `${dataEntry.title} ${Math.round(dataEntry.percentage)}%`
+            }
+            labelPosition={170 - lineWidth / 2}
+            labelStyle={{
+                fill: "#000",
+                opacity: 0.75,
+                pointerEvents: "none",
+            }}
+            onClick={(_, index) => {
+                setSelected(index === selected ? undefined : index);
+            }}
+            onMouseOver={(_, index) => {
+                setHovered(index);
+            }}
+            onMouseOut={() => {
+                setHovered(undefined);
+            }}
+        />
+    );
 }
 
 export default Pie;
